8d17b0b6ad600f6b7a4ec107438b48a545a09c7b
Amin <user0028@student.monash.edu>
2021-05-01 16:59:08 +0530

    Added Bush class

---
+++ src/game/Bush.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Bush extends Ground {
+    /**
+     * Constructor.
+     *
+     *
+     */
+    public Bush() {
+        super('b');
+    }
+}
