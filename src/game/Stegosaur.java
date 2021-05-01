2d1a4a28292c285e569ff412f641eb8d531989d0
Amin <user0028@student.monash.edu>
2021-05-01 17:03:18 +0530

    Updated Stegasaur class to utilize the newly added SeekFruitBehaviour class.

--- src/game/Stegosaur.java
+++ src/game/Stegosaur.java
@@ -14,18 +14,22 @@ import edu.monash.fit2099.engine.GameMap;
  */
 public class Stegosaur extends Actor {
 	// Will need to change this to a collection if Stegosaur gets additional Behaviours.
-	private Behaviour behaviour;
+	private Behaviour wBehaviour;
+	private Behaviour hBehaviour;
+	int foodLevel;
 
-	/** 
+	/**
 	 * Constructor.
 	 * All Stegosaurs are represented by a 'd' and have 100 hit points.
-	 * 
+	 *
 	 * @param name the name of this Stegosaur
 	 */
 	public Stegosaur(String name) {
 		super(name, 'd', 100);
-		
-		behaviour = new WanderBehaviour();
+
+		wBehaviour = new WanderBehaviour();
+		hBehaviour = new SeekFruitBehaviour();
+		foodLevel = 50;
 	}
 
 	@Override
@@ -35,19 +39,36 @@ public class Stegosaur extends Actor {
 
 	/**
 	 * Figure out what to do next.
-	 * 
+	 *
 	 * FIXME: Stegosaur wanders around at random, or if no suitable MoveActions are available, it
 	 * just stands there.  That's boring.
-	 * 
+	 *
 	 * @see edu.monash.fit2099.engine.Actor#playTurn(Actions, Action, GameMap, Display)
 	 */
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
-		Action wander = behaviour.getAction(this, map);
-		if (wander != null)
-			return wander;
-		
-		return new DoNothingAction();
+		// reduce food level each turn
+		this.foodLevel = this.foodLevel - 1;
+		if (this.foodLevel < 45){
+			System.out.println("Stegosaur is getting hungry!");
+			Action wander = hBehaviour.getAction(this, map);
+			if (wander != null)
+				return wander;
+			else{
+				return new DoNothingAction();
+			}
+		}
+		else{
+			Action wander = wBehaviour.getAction(this, map);
+			if (wander != null)
+				return wander;
+			else{
+				return new DoNothingAction();
+			}
+		}
+
+
+//		return new DoNothingAction();
 	}
 
 }
