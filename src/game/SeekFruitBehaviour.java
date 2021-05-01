290808bf91205ba312f0d6a410859520863624f5
Amin <user0028@student.monash.edu>
2021-05-01 21:44:37 +0530

    Updated SeekFruitBehaviour so that Stegosaurs move towards dropped fruits as well

--- src/game/SeekFruitBehaviour.java
+++ src/game/SeekFruitBehaviour.java
@@ -2,13 +2,16 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
-import java.util.ArrayList;
+
+import java.util.List;
+
 
 public class SeekFruitBehaviour implements Behaviour{
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
 
+        fallFruit(map);
         Location minLocation = map.locationOf(actor);
         Location closestFruit = closestFood(actor, map);
 
@@ -16,10 +19,20 @@ public class SeekFruitBehaviour implements Behaviour{
 //        Bush bush = (Bush) closestFruit.getGround();
 //        System.out.println(bush.getFruits());
 
-        // to test location of fruit
-//        System.out.println("x:"+closestFruit.x() + "y:" + closestFruit.y());
-        int minDistance = distance(closestFruit, map.locationOf(actor));
+        // to test location of closest fruit
+        System.out.println("x:"+closestFruit.x() + "y:" + closestFruit.y());
+
+        int minDistance = distance(closestFruit, minLocation);
         String name ="";
+
+        if (minDistance == 0){
+            // On same ground as fruit, so eating it
+            eatFood(actor, map, closestFruit);
+            // to test fruits in bush before eating
+//            System.out.println(bush.getFruits());
+
+        }
+
         for (Exit exit : map.locationOf(actor).getExits()) {
             Location destination = exit.getDestination();
             if (destination.canActorEnter(actor)) {
@@ -33,13 +46,7 @@ public class SeekFruitBehaviour implements Behaviour{
             }
         }
 
-        if (minDistance == 0){
-            // On same ground as fruit, so eating it
-            eatFood(actor, map, closestFruit);
-            // to test fruits in bush before eating
-//            System.out.println(bush.getFruits());
 
-        }
         return new MoveActorAction(minLocation, name);
 
     }
@@ -54,7 +61,7 @@ public class SeekFruitBehaviour implements Behaviour{
             for (int j : height){
                 Location newLocation = map.at(i, j);
                 if (newLocation.getGround() != null){
-                    if (newLocation.getGround().getDisplayChar() == 'b'){
+                    if (newLocation.getGround().getDisplayChar() == 'b') {
                         Bush currentBush = (Bush) newLocation.getGround();
                         if (currentBush.getFruits() > 0){
                             int distance = distance(stegLocation, newLocation);
@@ -63,11 +70,19 @@ public class SeekFruitBehaviour implements Behaviour{
                                 bestLocation = newLocation;
                             }
                         }
-
                     }
+                    List<Item> items = newLocation.getItems();
+                    for (Item item : items){
+                        if (item.getDisplayChar() == 'f'){
+                            int distance = distance(stegLocation, newLocation);
+                            if (distance < minDistance){
+                                minDistance = distance;
+                                bestLocation = newLocation;
+                            }
+                        }
+                    }
+
                 }
-
-
             }
         }
         return bestLocation;
@@ -80,7 +95,37 @@ public class SeekFruitBehaviour implements Behaviour{
     private void eatFood(Actor actor, GameMap map, Location fruitLocation){
         Stegosaur steg = (Stegosaur) actor;
         steg.increaseFoodLevel(10);
-        Bush bush = (Bush) fruitLocation.getGround();
-        bush.removeFruit();
+        if (fruitLocation.getGround().getDisplayChar() == 'b'){
+            Bush bush = (Bush) fruitLocation.getGround();
+            bush.removeFruit();
+        }
+        else {
+            List<Item> items = fruitLocation.getItems();
+
+//            Fruit fruit = new Fruit();
+            for (int i = 0; i<items.size(); i++ ){
+                if (items.get(i).getDisplayChar() == 'f'){
+                    fruitLocation.removeItem(items.get(i));
+                    System.out.println("eat");
+                }
+            }
+        }
+
+    }
+
+    public void fallFruit(GameMap map){
+        NumberRange width = map.getXRange();
+        NumberRange height = map.getYRange();
+        for (int i : width){
+            for (int j : height){
+                Location newLocation = map.at(i, j);
+                if (newLocation.getGround().getDisplayChar() == '+') {
+                    if (Math.random() < 0.05){
+                        Fruit fruit = new Fruit();
+                        newLocation.addItem(fruit);
+                    }
+                }
+            }
+        }
     }
 }
