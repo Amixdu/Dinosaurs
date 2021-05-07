6c2a9cc64ed2a36970cd8d0d8c73cc2813e189eb
Amin <user0028@student.monash.edu>
2021-05-08 00:50:35 +0530

    Created parent class 'VegetarianDinosaurs' so that Stegosaur and Brachiosaur can inherit from it

--- src/game/Stegosaur.java
+++ src/game/Stegosaur.java
@@ -12,13 +12,9 @@ import edu.monash.fit2099.engine.GameMap;
  * A herbivorous dinosaur.
  *
  */
-public class Stegosaur extends Actor {
+public class Stegosaur extends VegetarianDinosaur {
 	// Will need to change this to a collection if Stegosaur gets additional Behaviours.
-	private Behaviour wBehaviour;
-	private Behaviour hBehaviour;
-	int foodLevel;
-	int unconsciousCount;
-	int maxFoodLevel = 100;
+
 
 	/**
 	 * Constructor.
@@ -27,91 +23,12 @@ public class Stegosaur extends Actor {
 	 * @param name the name of this Stegosaur
 	 */
 	public Stegosaur(String name) {
-		super(name, 'd', 100);
+		super(name, 's', 100, 50, 100, 20, 90, 203);
 
-		wBehaviour = new WanderBehaviour();
-		hBehaviour = new SeekFruitBehaviour();
-		foodLevel = 50;
-		unconsciousCount = 0;
 	}
 
-	@Override
-	public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
-		Actions actions = new Actions();
-		actions.add(new AttackAction(this));
-		actions.add(new FeedStegosaurAction(this));
-		return actions;
-	}
-
-	/**
-	 * Figure out what to do next.
-	 *
-	 * FIXME: Stegosaur wanders around at random, or if no suitable MoveActions are available, it
-	 * just stands there.  That's boring.
-	 *
-	 * @see edu.monash.fit2099.engine.Actor#playTurn(Actions, Action, GameMap, Display)
-	 */
-	@Override
-	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
-
-//		System.out.println(this.foodLevel);
-
-		// reduce food level each turn
-		if (this.foodLevel > 0){
-			this.foodLevel = this.foodLevel - 1;
-			if (this.foodLevel < 90){
-				System.out.println("Stegosaur at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") is hungry!");
-				Action wander = hBehaviour.getAction(this, map);
-				if (wander != null)
-					return wander;
-				else{
-					return new DoNothingAction();
-				}
-			}
-			else{
-				Action wander = wBehaviour.getAction(this, map);
-				if (wander != null)
-					return wander;
-				else{
-					return new DoNothingAction();
-				}
-			}
-		}
-		else {
-			if (unconsciousCount < 20){
-				this.unconsciousCount += 1;
-				System.out.println("Stegosaur at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") is unconscious!");
-			}
-			else {
-				System.out.println("Stegosaur at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") died  due to lack of food!");
-				map.removeActor(this);
-			}
-
-			return new DoNothingAction();
-		}
 
 
 
-//		return new DoNothingAction();
-	}
-
-	public String getName(){
-		return this.name;
-	}
-
-	public int getFoodLevel() {
-		return foodLevel;
-	}
-
-	public void increaseFoodLevel(int amount){
-		this.foodLevel += amount;
-		if (this.foodLevel > maxFoodLevel){
-			this.foodLevel = maxFoodLevel;
-		}
-	}
-
-	public int getMaxFoodLevel(){
-		return this.maxFoodLevel;
-	}
 
 }
---
+++ src/game/VegetarianDinosaur.java
@@ -0,0 +1,114 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+
+public class VegetarianDinosaur extends Actor {
+    int foodLevel;
+    int maxFoodLevel;
+    int unconsciousCount;
+    int maxUconsciousRounds;
+    int hungerAmount;
+    int deathCount;
+    int maxDeadRounds;
+    private Behaviour wBehaviour;
+    private Behaviour hBehaviour;
+    /**
+     * Constructor.
+     *
+     * @param name        the name of the Actor
+     * @param displayChar the character that will represent the Actor in the display
+     * @param hitPoints   the Actor's starting hit points
+     */
+    public VegetarianDinosaur(String name, char displayChar, int hitPoints, int foodLevel, int maxFoodLevel, int maxUconsciousRounds, int hungerAmount, int maxDeadRounds) {
+        super(name, displayChar, hitPoints);
+        this.foodLevel = foodLevel;
+        this.maxFoodLevel = maxFoodLevel;
+        this.maxUconsciousRounds = maxUconsciousRounds;
+        this.hungerAmount = hungerAmount;
+        this.maxDeadRounds = maxDeadRounds;
+        wBehaviour = new WanderBehaviour();
+        hBehaviour = new SeekFruitBehaviour();
+
+    }
+
+
+
+    @Override
+    public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
+        Actions actions = new Actions();
+        actions.add(new AttackAction(this));
+        actions.add(new FeedVegetarianAction(this));
+        return actions;
+
+    }
+
+    @Override
+    public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+
+//		System.out.println(this.foodLevel);
+
+        // reduce food level each turn
+        if (this.foodLevel > 0){
+            this.foodLevel = this.foodLevel - 1;
+            if (this.foodLevel < hungerAmount){
+                System.out.println(this.name + "at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") is hungry!");
+                Action wander = hBehaviour.getAction(this, map);
+                if (wander != null)
+                    return wander;
+                else{
+                    return new DoNothingAction();
+                }
+            }
+            else{
+                Action wander = wBehaviour.getAction(this, map);
+                if (wander != null)
+                    return wander;
+                else{
+                    return new DoNothingAction();
+                }
+            }
+        }
+        else {
+            if (unconsciousCount < maxUconsciousRounds){
+                this.unconsciousCount += 1;
+                System.out.println(this.name + "at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") is unconscious!");
+            }
+            else {
+                System.out.println(this.name + "at (" + map.locationOf(this).x() + "," + map.locationOf(this).y() + ") died  due to lack of food!");
+//                map.removeActor(this);
+                if (deathCount < maxDeadRounds){
+                    deathCount += 1;
+                }
+                else{
+                    map.removeActor(this);
+                }
+
+            }
+
+            return new DoNothingAction();
+        }
+
+
+
+//		return new DoNothingAction();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getFoodLevel() {
+        return foodLevel;
+    }
+
+    public void increaseFoodLevel(int amount){
+        this.foodLevel += amount;
+        if (this.foodLevel > maxFoodLevel){
+            this.foodLevel = maxFoodLevel;
+        }
+    }
+
+    public int getMaxFoodLevel() {
+        return maxFoodLevel;
+    }
+}
