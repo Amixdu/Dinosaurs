8d39bc5f553f7a0b4712797e9b619853eb086ef4
Amin <user0028@student.monash.edu>
2021-05-08 10:29:25 +0530

    Updated the SeekFruitBehaviour class to use the heal() method when eating food

--- src/game/SeekFruitBehaviour.java
+++ src/game/SeekFruitBehaviour.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 
 public class SeekFruitBehaviour implements Behaviour{
+    char type;
+
+    public SeekFruitBehaviour(char type) {
+        this.type = type;
+    }
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
 
-//        fallFruit(map);
+        fallFruit(map);
         Location minLocation = map.locationOf(actor);
         Location closestFruit = closestFood(actor, map);
         if (closestFruit == null){
@@ -139,8 +144,11 @@ public class SeekFruitBehaviour implements Behaviour{
     }
 
     private String eatFood(Actor actor, GameMap map, Location fruitLocation){
-        Stegosaur steg = (Stegosaur) actor;
-        steg.increaseFoodLevel(10);
+        if (type == 'S'){
+            Stegosaur steg = (Stegosaur) actor;
+            steg.heal(10);
+        }
+
         if (fruitLocation.getGround().getDisplayChar() == 'b'){
             Bush bush = (Bush) fruitLocation.getGround();
             bush.removeFruit();
@@ -152,7 +160,7 @@ public class SeekFruitBehaviour implements Behaviour{
             for (int i = 0; i<items.size(); i++ ){
                 if (items.get(i).getDisplayChar() == 'f'){
                     fruitLocation.removeItem(items.get(i));
-                    System.out.println(steg.getName() + "eats");
+                    System.out.println("Stegasour at location (" + fruitLocation.x() + "," + fruitLocation.y() +")eats");
                 }
             }
         }
@@ -160,19 +168,19 @@ public class SeekFruitBehaviour implements Behaviour{
 
     }
 
-//    public void fallFruit(GameMap map){
-//        NumberRange width = map.getXRange();
-//        NumberRange height = map.getYRange();
-//        for (int i : width){
-//            for (int j : height){
-//                Location newLocation = map.at(i, j);
-//                if (newLocation.getGround().getDisplayChar() == '+') {
-//                    if (Math.random() < 0.05){
-//                        Fruit fruit = new Fruit();
-//                        newLocation.addItem(fruit);
-//                    }
-//                }
-//            }
-//        }
-//    }
+    public void fallFruit(GameMap map){
+        NumberRange width = map.getXRange();
+        NumberRange height = map.getYRange();
+        for (int i : width){
+            for (int j : height){
+                Location newLocation = map.at(i, j);
+                if (newLocation.getGround().getDisplayChar() == '+') {
+                    if (Math.random() < 0.05){
+                        Fruit fruit = new Fruit();
+                        newLocation.addItem(fruit);
+                    }
+                }
+            }
+        }
+    }
 }
