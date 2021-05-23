c80f1593c79718beb87221f3c1aafe5bd1778ab5
Amin <user0028@student.monash.edu>
2021-05-23 22:39:23 +0530

    Updated FeedCarnivore to give the user the option of feeding Corpses and Eggs in addition to Carnivore meal kits, to carnivorous dinosaurs

--- src/game/FeedCarnivoreAction.java
+++ src/game/FeedCarnivoreAction.java
@@ -6,6 +6,7 @@ import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Implement the FeedCarnivoreAction class
@@ -42,22 +43,61 @@ public class FeedCarnivoreAction extends Action {
         int beforeFeeding = carnivorousDinosaur.getHitPoints();
         int afterFeeding;
 
-
         // Cant feed if already full
         if (carnivorousDinosaur.getHitPoints() >= carnivorousDinosaur.getMaxHitPoints()){
             return (result + ", but " + carnivorousDinosaur.getName() + " is full!");
         }
 
-        // Going through inventory searching for carnivore meal kit, feed if found
+        // get users input
+        String userInput = userInput();
+
+        // Going through inventory searching for food, feed if found
         for (int i = 0; i < inventory.size(); i++){
-            if (inventory.get(i).getDisplayChar() == 'c'){
-                actor.removeItemFromInventory(inventory.get(i));
-                carnivorousDinosaur.heal(carnivorousDinosaur.getMaxHitPoints());
-                afterFeeding = carnivorousDinosaur.getHitPoints();
-                return (result + ", successfully fed (Hit points increased from " + beforeFeeding + " to " + afterFeeding + ")");
+            // feeding a corpse
+            if (userInput.equals("A")){
+                if (inventory.get(i).getDisplayChar() == 'C'){
+                    Corpse corpse = (Corpse) inventory.get(i);
+                    int points = corpse.getCorpsePoints();
+                    actor.removeItemFromInventory(inventory.get(i));
+                    carnivorousDinosaur.heal(points);
+                    afterFeeding = carnivorousDinosaur.getHitPoints();
+                    return (result + ", successfully fed (Hit points increased from " + beforeFeeding + " to " + afterFeeding + ")");
+                }
             }
+            // feeding a carnivore meal kit
+            else if(userInput.equals("B")){
+                if (inventory.get(i).getDisplayChar() == 'c'){
+                    actor.removeItemFromInventory(inventory.get(i));
+                    carnivorousDinosaur.heal(carnivorousDinosaur.getMaxHitPoints());
+                    afterFeeding = carnivorousDinosaur.getHitPoints();
+                    return (result + ", successfully fed (Hit points increased from " + beforeFeeding + " to " + afterFeeding + ")");
+                }
+            }
+            // feeding an egg
+            else if (userInput.equals("C")){
+                char displayChar = inventory.get(i).getDisplayChar();
+                if (displayChar == 'q' || displayChar == 'w' || displayChar == 'e' || displayChar == 'y'){
+                    actor.removeItemFromInventory(inventory.get(i));
+                    carnivorousDinosaur.heal(10);
+                    afterFeeding = carnivorousDinosaur.getHitPoints();
+                    return (result + ", successfully fed (Hit points increased from " + beforeFeeding + " to " + afterFeeding + ")");
+                }
+            }
+
         }
-        return ("No carnivore carnivore meal kits in inventory");
+        return ("Item not in inventory");
+    }
+
+    /**
+     * Method to get user input on whether to feed fruit or vegetarian meal kit
+     * @return string represnting user input
+     */
+    private String userInput(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press A to feed Corpse\nPress B to feed carnivore meal kit\nPress C to feed egg");
+        String result = scanner.next();
+        return result;
+
     }
 
     /**
