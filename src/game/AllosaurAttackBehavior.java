0f8a5c83379de10ec558db68abeced3801662981
Amin <user0028@student.monash.edu>
2021-05-25 14:43:07 +0530

    Updated AllosaurAttackBehavior to reuse existing code

--- src/game/AllosaurAttackBehavior.java
+++ src/game/AllosaurAttackBehavior.java
@@ -16,34 +16,38 @@ public class AllosaurAttackBehavior implements Behaviour{
      */
     @Override
     public Action getAction(Actor actor, GameMap map) {
-        Location current = map.locationOf(actor);
-        SearchMap searchMap = new SearchMap(actor, map);
-        Location stegLocation = searchMap.closest('S', "Actor");
-        Location pteroLocation = searchMap.closest('P', "Actor");
-        // if attacking a stegosaur is possible, an AllosaurAttackAction will be returned
-        if (stegLocation != null){
-            Stegosaur target = (Stegosaur) stegLocation.getActor();
-            if (target.isConscious()){
-                if (target.getAttackCoolDown() >= 20){
-                    if (distance(current, stegLocation) == 1){
-                        target.resetAttackCoolDown();
-                        Dinosaur dino = (Dinosaur) actor;
-                        int damage = 20;
-                        // if attacker is a baby allosaur, only do 10 damage and heal 10 points
-                        if (dino.getAgeGroup() == AgeGroup.BABY)
-                            damage = 10;
-                        return (new AllosaurAttackAction(actor, target, damage, damage));
+        Location actorLocation = map.locationOf(actor);
+
+        // if adjacent and not pregnant -> Mate
+        for (Exit e : actorLocation.getExits()){
+            if (e.getDestination().containsAnActor()){
+                Actor adjacentActor =  e.getDestination().getActor();
+                if (adjacentActor.getDisplayChar() == 'P' || adjacentActor.getDisplayChar() == 'p'){
+                    Pterodactyl target = (Pterodactyl) adjacentActor;
+                    // if Pterodactyl is flying, it is safe from allosaur attacks
+                    if (target.hasCapability(Flight.YES)){
+                        System.out.println(actor.toString() + " spots " + target.toString() + " nearby, but it is flying!");
+                        return null;
+                    }
+                    // pterodactyl is conscious
+                    if ((target.isConscious())){
+                        return (new AllosaurEatPterodactylAction(actor, target));
                     }
                 }
-            }
-        }
-        // if attacking a Pterodactyl is possible, an AllosaurEatPterodactylAction will be returned
-        if (pteroLocation != null){
-            Pterodactyl target = (Pterodactyl) pteroLocation.getActor();
-            // pterodactyl is conscious and not flying
-            if ((target.isConscious()) && target.hasCapability(Flight.NO)){
-                if (distance(current, pteroLocation) == 1){
-                    return (new AllosaurEatPterodactylAction(actor, target));
+                if (adjacentActor.getDisplayChar() == 'S' || adjacentActor.getDisplayChar() == 's'){
+                    // if attacking a stegosaur is possible, an AllosaurAttackAction will be returned
+                    Stegosaur target = (Stegosaur) adjacentActor;
+                    if (target.isConscious()){
+                        if (target.getAttackCoolDown() >= 20){
+                            target.resetAttackCoolDown();
+                            Dinosaur dino = (Dinosaur) actor;
+                            int damage = 20;
+                            // if attacker is a baby allosaur, only do 10 damage and heal 10 points
+                            if (dino.getAgeGroup() == AgeGroup.BABY)
+                                damage = 10;
+                            return (new AllosaurAttackAction(actor, target, damage, damage));
+                        }
+                    }
                 }
             }
         }
